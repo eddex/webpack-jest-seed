eac6bc87d86e5994beaa9edd574d0d595705251f
eddex <eddex@outlook.com>
2019-09-02 14:05:01 +0200

    implement multiplyNumeric() function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,13 @@
-console.log('Your code here.');
+function multiplyNumeric(menu) {
+    
+    for (let key in menu) {
+        
+        if (typeof menu[key] === 'number') {
+            menu[key] = menu[key] * 2;
+        } 
+    }
+}
+
+let menu = { width: 200, height: 300, title: "Titel" };
+multiplyNumeric(menu);
+console.log(menu);
