051dcc21524f4c9388cad84c99ee5ced962addaf
eddex <eddex@outlook.com>
2019-09-03 09:26:07 +0200

    DOM exercises

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,3 +11,25 @@ function multiplyNumeric(menu) {
 let menu = { width: 200, height: 300, title: "Titel" };
 multiplyNumeric(menu);
 console.log(menu);
+
+document.body.getElementsByTagName('h1')[0].style = 'color: red;';
+
+let liElements = document.body.getElementsByTagName('ul')[0].getElementsByTagName('li');
+
+// for .. in selects the indexes
+for (index in liElements) {
+    liElements[index].style = 'color: green;';
+}
+
+// for .. of directly selects the elements
+for (liElement of liElements) {
+    liElement.style['font-size'] = '2em';
+
+    if (liElement.innerText === 'Bruno') {
+        liElement.style.color = 'red';
+    }
+}
+
+console.log(document.body.getElementsByTagName('div')[1].getAttribute('data-hslu-module'));
+document.body.getElementsByTagName('div')[1].setAttribute('data-hslu-module', 'WEBLAB');
+console.log(document.body.getElementsByTagName('div')[1].getAttribute('data-hslu-module'));
