ecdb2832182b493da967e0969fe557c1d74cb2b7
eddex <eddex@outlook.com>
2019-09-03 09:51:29 +0200

    exercise: generate list from object

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,13 @@
+/*
+ * Exercise: Multiply Numeric
+ */
 function multiplyNumeric(menu) {
-    
+
     for (let key in menu) {
-        
+
         if (typeof menu[key] === 'number') {
             menu[key] = menu[key] * 2;
-        } 
+        }
     }
 }
 
@@ -12,17 +15,24 @@ let menu = { width: 200, height: 300, title: "Titel" };
 multiplyNumeric(menu);
 console.log(menu);
 
+/*
+ * Exercise: Select h1 element
+ */
 document.body.getElementsByTagName('h1')[0].style = 'color: red;';
 
+
+/*
+ * Exercise: Select all li elements
+ */
 let liElements = document.body.getElementsByTagName('ul')[0].getElementsByTagName('li');
 
 // for .. in selects the indexes
-for (index in liElements) {
+for (const index in liElements) {
     liElements[index].style = 'color: green;';
 }
 
 // for .. of directly selects the elements
-for (liElement of liElements) {
+for (const liElement of liElements) {
     liElement.style['font-size'] = '2em';
 
     if (liElement.innerText === 'Bruno') {
@@ -30,6 +40,44 @@ for (liElement of liElements) {
     }
 }
 
+/*
+ * Exercise: Select and modify custom attribute
+ */
 console.log(document.body.getElementsByTagName('div')[1].getAttribute('data-hslu-module'));
 document.body.getElementsByTagName('div')[1].setAttribute('data-hslu-module', 'WEBLAB');
 console.log(document.body.getElementsByTagName('div')[1].getAttribute('data-hslu-module'));
+
+/*
+ * Exercise: Generate list from object
+ */
+let techRadar = {
+    "Meta": {
+        "generatedAt": new Date()
+    },
+    "TechRadar": {
+        "Techniques": ["Micro Frontends", "Polyglot programming", "Secrets as a service", "Chaos Engineering"],
+        "Tools": ["Cypress", "Helm", "Traefik", "Humio"], 
+        "Platforms": ["Contentful", "AWS Fargate", "InfluxDB"], 
+        "Languages & Frameworks": ["TypeScript", "Rust"]
+    }
+};
+
+let p = document.createElement('p');
+p.innerText = "Generated at " + techRadar.Meta.generatedAt;
+document.body.appendChild(p);
+
+let list = document.createElement('ul');
+for (const title in techRadar.TechRadar) {
+    let liTitle = document.createElement('li');
+    liTitle.innerText = title;
+    
+    let entries = document.createElement('ul');
+    for (const entry of techRadar.TechRadar[title]) {
+        let liEntry = document.createElement('li');
+        liEntry.innerText = entry;
+        entries.appendChild(liEntry);
+    }
+    liTitle.appendChild(entries);
+    list.appendChild(liTitle);
+}
+document.body.appendChild(list);
